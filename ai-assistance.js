6c23612e3099ea9730b2832c0c3e889f1a8d6622
Isaac Yanez <isaac231467@gmail.com>
2023-04-20 18:59:25 -0500

    fixed the ai prompt

--- ai-assistance.js
+++ ai-assistance.js
@@ -19,7 +19,7 @@ app.post("/ai-assistance", async (req, res) => {
     const openai = new OpenAIApi(configuration);
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt} ${codeSnippet}`,
+      prompt: `You will be receiving a prompt from a user about helping regardning programing if it is not code related please let the user know you are only built to answer questions regarding programing once you have checked to see the user is asking questions about programming you will only lead the user to the question do not answer the question right away. This is the prompt ${prompt} and this is the code snippet ${codeSnippet}`,
       temperature: 1,
       max_tokens: 4000,
     });
